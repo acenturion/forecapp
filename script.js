a84e4e5ee9a19a639491b23ae3029017c476edbb
acenturion <alejandrocenturion@outlook.com>
2018-04-18 22:37:41 -0300

    Update: new Date()
    +New system hour
    +Secret code :ox:
    + Clean spaguetti code

--- script.js
+++ script.js
@@ -16,19 +16,26 @@ function fixLocation(location){
 
 function setFondo(){
 
-	console.log("entre");
 	var width = $(window).width();
 	var height = $(window).height();
 	var imageUrl = `https://picsum.photos/g/${width}/${height}?random`;
- 	console.log(imageUrl);
 	$('body').css('background-image', 'url(' + imageUrl + ')');
 }
 
-$(document).ready(function(){
+ function setDate() {
+    var now = new Date();
+    var seconds = now.getSeconds();
+    var mins = now.getMinutes();
+    var hour = now.getHours();
+   	
+   	if(hour < 10){ hour = "0"+hour};
+   	if(mins < 10){ mins = "0"+mins};
+   	if(seconds < 10){ seconds = "0"+seconds};
 
-	setFondo();
+   	$(".time").html(`<p><strong>${hour}:${mins}:${seconds}<p><strong>`);
+  }
 
-	//-34.586976, -58.606536
+  function setData(){
 	$.ajax({
 		method:"GET",
 		url: "https://api.darksky.net/forecast/a7ce4bf352c37f9df06cf2b8f8e699fc/-34.586976,-58.606536?units=auto",
@@ -37,14 +44,46 @@ $(document).ready(function(){
       		request.setRequestHeader("Access-Control-Allow-Origin", '*');
     	},
 		success: function(response){
-			$(".temperatura").html("<p><strong>"+fixLocation(response.timezone)+"</strong></p>");
-			$(".temperatura").append("<p><strong>"+unixTime(parseInt(response.currently.time))+"</strong></p>");
+			$(".location").append("<p><strong>"+fixLocation(response.timezone)+"</strong></p>");
 			$(".temperatura").append("<p><strong>Temperatura: "+parseInt(response.currently.temperature)+"&#186;</strong></p>");
-			$(".temperatura").append("<p><strong>Humedad: "+(response.currently.humidity*100)+"%</strong></p>");
+			$(".humedad").append("<p><strong>Humedad: "+(response.currently.humidity*100)+"%</strong></p>");
 		}
 	});
+  }
+
+  function secretCode(){
+
+  	const pressed = [];
+	const secretCode = "mateiko"
+
+	window.addEventListener("keyup", (e) =>{
+		pressed.push(e.key);
+		pressed.splice(-secretCode.length - 1, pressed.length -secretCode.length);
+
+		console.log(pressed);
+
+		if(pressed.join("").includes(secretCode)){
+			cornify_add();
+		}
+	});
+
+  }
+  setFondo();
+  setData();
+  setDate();
+  secretCode();
+
+$(document).ready(function(){
+
+	setInterval(setDate, 1000);
 
 });
 
 
-//https://picsum.photos/g/200/300?random
+
+ 
+
+  
+
+  
+
